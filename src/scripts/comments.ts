9cd65c0d14dca18baf0276f7f08368b94d934e0e
Kaleb <kalebzambrano@outlook.com>
2025-08-31 00:11:31 -0500

    agregando nuevos apartados de presentación

---
+++ src/scripts/comments.ts
@@ -0,0 +1,17 @@
+const COMMENTS = [
+    {
+        id: 1,
+        name: "Fernando",
+        comment: "Desde el segundo ciclo no llevo un refuerzo a mis cursos"
+    },
+    {
+        id: 2,
+        name: "Luis",
+        comment: "Despues de Fisica II no llevo un refuerzo, pues ya no hay para los cursos que tengo actualmente"
+    },
+    {
+        id: 3,
+        name: "Rodrigo",
+        comment: ""
+    }
+]
